f046da14bf288e52d508e94255fc396fce73c58e
minji05 <smj95133@likelion.org>
2023-10-09 18:26:16 +0900

    [FEAT] : ⚙️ SignUpActivity 구현 완료

--- app/src/main/java/com/example/self_introduction/SignUpActivity.kt
+++ app/src/main/java/com/example/self_introduction/SignUpActivity.kt
@@ -1,11 +1,29 @@
 package com.example.self_introduction
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.self_introduction.databinding.ActivitySignUpBinding
 
+
+private lateinit var binding: ActivitySignUpBinding
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnSignIn.setOnClickListener(){
+
+            if(binding.tvName.text.toString().trim().isEmpty() || binding.tvId.text.trim().isEmpty() || binding.tvPw.text.trim().isEmpty()) {
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다.",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+                }
+
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
