f9e48ed9c852ba152455b98168183983728d41f0
minji05 <smj95133@likelion.org>
2023-10-09 17:40:40 +0900

    [FEAT] : ⚙️ 공백 체크 후 토스트 메세지 띄우기

--- app/src/main/java/com/example/self_introduction/SignInActivity.kt
+++ app/src/main/java/com/example/self_introduction/SignInActivity.kt
@@ -3,7 +3,9 @@ package com.example.self_introduction
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
+import android.widget.Toast
 import com.example.self_introduction.databinding.ActivitySignInBinding
 
 
@@ -15,20 +17,29 @@ class SignInActivity : AppCompatActivity() {
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLogin.setOnClickListener(){
+        binding.btnLogin.setOnClickListener() {
 
-            val strData = binding.tvId.text.toString()
-            val intent = Intent(this, HomeActivity::class.java)
-            intent.putExtra("activity",strData)
-            startActivity(intent)
+            if (binding.tvId.text.toString().trim().isEmpty() || binding.tvPw.text.toString().trim()
+                    .isEmpty()
+            ) {
+                Toast.makeText(this, "아이디/비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
 
-        }
 
-        binding.btnSign.setOnClickListener(){
+                Log.d("로그인 로그", "로그인 성공")
+                val strData = binding.tvId.text.toString()
+                val intent = Intent(this, HomeActivity::class.java)
+                intent.putExtra("activity", strData)
+                startActivity(intent)
 
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            }
 
+            binding.btnSign.setOnClickListener() {
+
+                val intent = Intent(this, SignUpActivity::class.java)
+                startActivity(intent)
+
+            }
         }
     }
 }
